99009f1e3070d3769614ec8f1750debeefff6453
rareed2021 <rareed20@outlook.com>
2021-04-25 20:58:08 -0500

    Made base ViewModel abstract

--- app/src/main/java/com/b29/frankenstein/ui/common/BaseViewModel.kt
+++ app/src/main/java/com/b29/frankenstein/ui/common/BaseViewModel.kt
@@ -4,7 +4,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import io.reactivex.rxjava3.disposables.CompositeDisposable
 
-class BaseViewModel : ViewModel() {
+abstract class BaseViewModel : ViewModel() {
     val errorText = MutableLiveData<String>("")
     val disposables = CompositeDisposable()
 }
